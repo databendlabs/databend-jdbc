716f83de0ee0d1c9f77782517ecacde91ebd4891
hantmac <hantmac@outlook.com>
2023-07-05 17:36:11 +0800

    fix: add column builder and reformat code

--- databend-jdbc/src/main/java/com/databend/jdbc/DatabendColumnInfo.java
+++ databend-jdbc/src/main/java/com/databend/jdbc/DatabendColumnInfo.java
@@ -2,6 +2,8 @@ package com.databend.jdbc;
 
 import com.databend.client.data.DatabendRawType;
 import com.databend.client.data.DatabendTypes;
+import com.google.common.base.Preconditions;
+import com.google.common.base.Strings;
 import com.google.common.collect.ImmutableList;
 
 import java.sql.Types;
@@ -10,8 +12,7 @@ import java.util.Locale;
 
 import static java.util.Objects.requireNonNull;
 
-public class DatabendColumnInfo
-{
+public class DatabendColumnInfo {
     private static final int VARBINARY_MAX = 1024 * 1024 * 1024;
     private static final int TIME_ZONE_MAX = 40; // current longest time zone is 32
     private static final int TIME_MAX = "HH:mm:ss.SSS".length();
@@ -21,6 +22,7 @@ public class DatabendColumnInfo
     private static final int DATE_MAX = "yyyy-MM-dd".length();
 
     private final int columnType;
+    private final String columnName;
     private final List<Integer> columnParameterTypes;
     private final DatabendRawType type;
     private final Nullable nullable;
@@ -30,13 +32,12 @@ public class DatabendColumnInfo
     private final int scale;
     private final int columnDisplaySize;
     private final String columnLabel;
-    private final String columnName;
+
     private final String tableName;
     private final String schemaName;
     private final String catalogName;
 
-    public DatabendColumnInfo(int columnType, List<Integer> columnParameterTypes, DatabendRawType type, Nullable nullable, boolean currency, boolean signed, int precision, int scale, int columnDisplaySize, String columnLabel, String columnName, String tableName, String schemaName, String catalogName)
-    {
+    public DatabendColumnInfo(int columnType, List<Integer> columnParameterTypes, DatabendRawType type, Nullable nullable, boolean currency, boolean signed, int precision, int scale, int columnDisplaySize, String columnLabel, String columnName, String tableName, String schemaName, String catalogName) {
         this.columnType = columnType;
         this.columnParameterTypes = columnParameterTypes;
         this.type = type;
@@ -53,8 +54,12 @@ public class DatabendColumnInfo
         this.catalogName = catalogName;
     }
 
-    public static void setTypeInfo(Builder builder, DatabendRawType type)
-    {
+    public static DatabendColumnInfo of(String name, DatabendRawType type) {
+        Preconditions.checkArgument(!Strings.isNullOrEmpty(name), "Provided name is null or empty");
+        return newBuilder(name, type).build();
+    }
+
+    public static void setTypeInfo(Builder builder, DatabendRawType type) {
         builder.setColumnType(getType(type));
         boolean isNullable = type.isNullable();
         builder.setNullable(isNullable ? Nullable.NULLABLE : Nullable.NO_NULLS);
@@ -151,8 +156,12 @@ public class DatabendColumnInfo
 
     }
 
+    public static Builder newBuilder(String name, DatabendRawType type) {
+        return (new Builder()).setColumnName(name).setColumnType(getType(type));
+    }
+
     private static int getType(DatabendRawType type) {
-        if (type ==null) {
+        if (type == null) {
             return java.sql.Types.NULL;
         }
 //        if (type.isNullable()) {
@@ -198,90 +207,73 @@ public class DatabendColumnInfo
         }
     }
 
-    public int getColumnType()
-    {
+    public int getColumnType() {
         return columnType;
     }
 
-    public List<Integer> getColumnParameterTypes()
-    {
+    public List<Integer> getColumnParameterTypes() {
         return columnParameterTypes;
     }
 
-    public DatabendRawType getType()
-    {
+    public DatabendRawType getType() {
         return type;
     }
 
-    public Nullable getNullable()
-    {
+    public Nullable getNullable() {
         return nullable;
     }
 
-    public String getColumnTypeName()
-    {
+    public String getColumnTypeName() {
         return type.toString();
     }
 
-    public boolean isCurrency()
-    {
+    public boolean isCurrency() {
         return currency;
     }
 
-    public boolean isSigned()
-    {
+    public boolean isSigned() {
         return signed;
     }
 
-    public int getPrecision()
-    {
+    public int getPrecision() {
         return precision;
     }
 
-    public int getScale()
-    {
+    public int getScale() {
         return scale;
     }
 
-    public int getColumnDisplaySize()
-    {
+    public int getColumnDisplaySize() {
         return columnDisplaySize;
     }
 
-    public String getColumnLabel()
-    {
+    public String getColumnLabel() {
         return columnLabel;
     }
 
-    public String getColumnName()
-    {
+    public String getColumnName() {
         return columnName;
     }
 
-    public String getTableName()
-    {
+    public String getTableName() {
         return tableName;
     }
 
-    public String getSchemaName()
-    {
+    public String getSchemaName() {
         return schemaName;
     }
 
-    public String getCatalogName()
-    {
+    public String getCatalogName() {
         return catalogName;
     }
 
 
-    public enum Nullable
-    {
+    public enum Nullable {
         NO_NULLS, NULLABLE, UNKNOWN
     }
 
     // builder
-    static class Builder
-    {
+    public static final class Builder {
         private int columnType;
         private List<Integer> columnParameterTypes;
         private DatabendRawType type;
@@ -297,91 +289,96 @@ public class DatabendColumnInfo
         private String schemaName;
         private String catalogName;
 
-        public Builder setColumnType(int columnType)
-        {
+        Builder() {
+        }
+
+        private Builder(DatabendColumnInfo databendColumnInfo) {
+            this.columnName = databendColumnInfo.columnName;
+            this.columnType = databendColumnInfo.columnType;
+            this.columnLabel = databendColumnInfo.columnLabel;
+            this.type = databendColumnInfo.type;
+            this.columnDisplaySize = databendColumnInfo.columnDisplaySize;
+            this.tableName = databendColumnInfo.tableName;
+            this.schemaName = databendColumnInfo.schemaName;
+            this.catalogName = databendColumnInfo.catalogName;
+            this.scale = databendColumnInfo.scale;
+            this.columnParameterTypes = databendColumnInfo.columnParameterTypes;
+            this.signed = databendColumnInfo.signed;
+            this.currency = databendColumnInfo.currency;
+            this.nullable = databendColumnInfo.nullable;
+        }
+
+
+        public Builder setColumnType(int columnType) {
             this.columnType = columnType;
             return this;
         }
 
-        public void setColumnParameterTypes(List<Integer> columnParameterTypes)
-        {
+        public void setColumnParameterTypes(List<Integer> columnParameterTypes) {
             this.columnParameterTypes = ImmutableList.copyOf(requireNonNull(columnParameterTypes, "columnParameterTypes is null"));
         }
 
-        public Builder setColumnTypeSignature(DatabendRawType columnTypeSignature)
-        {
+        public Builder setColumnTypeSignature(DatabendRawType columnTypeSignature) {
             this.type = columnTypeSignature;
             return this;
         }
 
-        public Builder setNullable(Nullable nullable)
-        {
+        public Builder setNullable(Nullable nullable) {
             this.nullable = nullable;
             return this;
         }
 
-        public Builder setCurrency(boolean currency)
-        {
+        public Builder setCurrency(boolean currency) {
             this.currency = currency;
             return this;
         }
 
-        public Builder setSigned(boolean signed)
-        {
+        public Builder setSigned(boolean signed) {
             this.signed = signed;
             return this;
         }
 
-        public Builder setPrecision(int precision)
-        {
+        public Builder setPrecision(int precision) {
             this.precision = precision;
             return this;
         }
 
-        public Builder setScale(int scale)
-        {
+        public Builder setScale(int scale) {
             this.scale = scale;
             return this;
         }
 
-        public Builder setColumnDisplaySize(int columnDisplaySize)
-        {
+        public Builder setColumnDisplaySize(int columnDisplaySize) {
             this.columnDisplaySize = columnDisplaySize;
             return this;
         }
 
-        public Builder setColumnLabel(String columnLabel)
-        {
+        public Builder setColumnLabel(String columnLabel) {
             this.columnLabel = columnLabel;
             return this;
         }
 
-        public Builder setColumnName(String columnName)
-        {
+        public Builder setColumnName(String columnName) {
             this.columnName = columnName;
             return this;
         }
 
-        public Builder setTableName(String tableName)
-        {
+        public Builder setTableName(String tableName) {
             this.tableName = tableName;
             return this;
         }
 
-        public Builder setSchemaName(String schemaName)
-        {
+        public Builder setSchemaName(String schemaName) {
             this.schemaName = schemaName;
             return this;
         }
 
-        public Builder setCatalogName(String catalogName)
-        {
+        public Builder setCatalogName(String catalogName) {
             this.catalogName = catalogName;
             return this;
         }
 
-        public DatabendColumnInfo build()
-        {
+        public DatabendColumnInfo build() {
             return new DatabendColumnInfo(
                     columnType,
                     columnParameterTypes,
