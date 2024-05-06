34c5022146e487f12bdd2857741e1e0ebd3fc472
hantmac <hantmac@outlook.com>
2024-05-06 21:06:35 +0800

    feat: add DatabendConnection pool factory examples

---
+++ databend-jdbc/src/main/java/com/databend/jdbc/examples/DatabendConnectionFactory.java
@@ -0,0 +1,55 @@
+package com.databend.jdbc.examples;
+import com.databend.jdbc.DatabendConnection;
+import com.fasterxml.jackson.databind.annotation.JsonAppend;
+import org.apache.commons.pool2.PooledObjectFactory;
+import org.apache.commons.pool2.PooledObject;
+import org.apache.commons.pool2.impl.DefaultPooledObject;
+import org.apache.commons.pool2.impl.GenericObjectPool;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DatabendConnectionFactory implements PooledObjectFactory<DatabendConnection> {
+
+    private  String url;
+    private Properties properties;
+
+    public DatabendConnectionFactory(String url, Properties properties) {
+        this.url = url;
+        this.properties = properties;
+    }
+    private Connection createConnection(String url, Properties p) throws SQLException {
+        return DriverManager.getConnection(url, p);
+    }
+
+    @Override
+    public PooledObject<DatabendConnection> makeObject() throws Exception {
+        DatabendConnection connection = (DatabendConnection) createConnection(url, properties);
+        return new DefaultPooledObject<>(connection);
+    }
+
+    @Override
+    public void destroyObject(PooledObject<DatabendConnection> p) throws Exception {
+        p.getObject().close();
+    }
+
+    @Override
+    public boolean validateObject(PooledObject<DatabendConnection> p) {
+        try {
+            return !p.getObject().isClosed();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void activateObject(PooledObject<DatabendConnection> p) throws Exception {
+    }
+
+    @Override
+    public void passivateObject(PooledObject<DatabendConnection> p) throws Exception {
+    }
+}
+
---
+++ databend-jdbc/src/main/java/com/databend/jdbc/examples/DatabendConnectionPool.java
@@ -0,0 +1,35 @@
+package com.databend.jdbc.examples;
+
+import com.databend.jdbc.DatabendConnection;
+import org.apache.commons.pool2.impl.GenericObjectPool;
+
+import java.util.Properties;
+
+public class DatabendConnectionPool extends GenericObjectPool<DatabendConnection> {
+    public DatabendConnectionPool(DatabendConnectionFactory factory) {
+        super(factory);
+    }
+
+    public void testDemo() throws Exception {
+        Properties props = new Properties();
+        props.setProperty("database", "db3");
+        props.setProperty("SSL", "false");
+        // Create a Databend connection pool
+        DatabendConnectionFactory factory = new DatabendConnectionFactory("jdbc:databend://localhost:8000", props);
+        DatabendConnectionPool pool = new DatabendConnectionPool(factory);
+
+        // Get a connection from the pool
+        DatabendConnection connection = pool.borrowObject();
+
+        // Use the connection
+        connection.createStatement().executeQuery("SELECT version()");
+
+        // Return the connection to the pool
+        pool.returnObject(connection);
+
+        // Close the pool when done
+        pool.close();
+    }
+}
+
+
