9de1e8cf4356a378269acc32ba629549916d85f1
Yang Xiufeng <yangxiufeng.c@gmail.com>
2024-11-13 00:07:15 +0800

    test temp table

---
+++ databend-jdbc/src/test/java/com/databend/jdbc/TestTempTable.java
@@ -0,0 +1,28 @@
+package com.databend.jdbc;
+
+import org.junit.Assert;
+import org.testng.annotations.Test;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TestTempTable {
+
+    @Test
+    public void testTempTable() throws SQLException {
+        try(Connection c1 = Utils.createConnection()) {
+            Statement statement= c1.createStatement();
+            statement.execute("create or replace temp table table1(i int)");
+            statement.execute("insert into table1 values (1), (2)");
+            statement.executeQuery("select * from table1");
+            ResultSet rs = statement.getResultSet();
+            Assert.assertEquals(true, rs.next());
+            Assert.assertEquals(1, rs.getInt(1));
+            Assert.assertEquals(true, rs.next());
+            Assert.assertEquals(2, rs.getInt(1));
+            Assert.assertEquals(false, rs.next());
+        }
+    }
+}
