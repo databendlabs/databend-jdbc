7de85b530fcf000d0d7a26deb3c04e33b61bde59
Yang Xiufeng <yangxiufeng.c@gmail.com>
2025-03-24 10:04:38 +0800

    update server version for test.

--- databend-client/src/main/java/com/databend/client/ServerVersions.java
+++ databend-client/src/main/java/com/databend/client/ServerVersions.java
@@ -3,7 +3,7 @@ package com.databend.client;
 import com.github.zafarkhaja.semver.Version;
 
 public class ServerVersions {
-    private static final Version HEARTBEAT = Version.forIntegers(1, 2, 708);
+    private static final Version HEARTBEAT = Version.forIntegers(1, 2, 709);
 
     public static boolean supportHeartbeat(Version ver) {
         return ver != null && ver.greaterThan(HEARTBEAT);
