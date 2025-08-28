c70305fe28d34c0d48865363b30b97aece472b9d
Yang Xiufeng <yangxiufeng.c@gmail.com>
2025-08-29 00:50:38 +0800

    test older server

--- databend-jdbc/src/test/java/com/databend/jdbc/Compatibility.java
+++ databend-jdbc/src/test/java/com/databend/jdbc/Compatibility.java
@@ -2,11 +2,6 @@ package com.databend.jdbc;
 
 import com.vdurmont.semver4j.Semver;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.Properties;
-
 public class Compatibility {
     public static class Capability {
         boolean streamingLoad;
@@ -19,35 +14,34 @@ public class Compatibility {
         }
 
         public static Capability fromServerVersion(Semver ver) {
-            boolean streamingLoad =  ver.isGreaterThan(new Semver("1.2.781"));
+            boolean streamingLoad =  ver.isGreaterThanOrEqualTo(new Semver("1.2.792"));
             return new Capability(streamingLoad);
         }
 
         public static Capability fromDriverVersion(Semver ver) {
-            boolean streamingLoad =  ver.isGreaterThan(new Semver("0.4.1"));
+            boolean streamingLoad =  ver.isGreaterThanOrEqualTo(new Semver("0.4.1"));
             return new Capability(streamingLoad);
         }
     }
 
-    static String port = System.getenv("DATABEND_TEST_CONN_PORT") != null ? System.getenv("DATABEND_TEST_CONN_PORT").trim() : "8000";
     public static Semver driverVersion = getDriverVersion();
     public static Semver serverVersion = getServerVersion();
     public static Capability driverCapability = driverVersion==null? new Capability(): Capability.fromDriverVersion(driverVersion);
     public static Capability serverCapability = serverVersion==null? new Capability(): Capability.fromServerVersion(serverVersion);
 
     private static Semver getDriverVersion() {
-        String env = System.getenv("DATABEND_TEST_DRIVER_VERSION");
+        String env = System.getenv("DATABEND_JDBC_VERSION");
         if (env == null) {
             return null;
         }
         return new Semver(env);
     }
     private static Semver getServerVersion() {
-        String env = System.getenv("DATABEND_TEST_SERVER_VERSION");
-        if (env == null) {
+        String env = System.getenv("DATABEND_QUERY_VERSION");
+        if (env == null || "nightly".equals(env)) {
             return null;
         }
-        return new Semver(env);
+        return new Semver(env, Semver.SemverType.NPM).withClearedSuffixAndBuild();
     }
 
      public static boolean skipDriverBug(String version) {
