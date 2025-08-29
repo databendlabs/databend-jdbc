451943b94b4c877af74f427492597395c33710e2
everpcpc <git@everpcpc.com>
2025-08-29 18:08:08 +0800

    ci: test compatibility in PR

--- tests/compatibility/test_compatibility.sh
+++ tests/compatibility/test_compatibility.sh
@@ -20,18 +20,11 @@ TEST_SIDE=${TEST_SIDE:-server}
 TEST_VER=${DATABEND_JDB_TEST_VERSION:-$CURRENT_VERSION}
 JDBC_VER=${DATABEND_JDBC_VERSION:-$CURRENT_VERSION}
 
-if [ "$TEST_SIDE" = "server" ]; then
-    curl -sSLfo ./databend-jdbc-tests.jar "https://github.com/databendlabs/databend-jdbc/releases/download/v${TEST_VER}/databend-jdbc-${TEST_VER}-tests.jar"
-else
-    cp ../../databend-jdbc/target/databend-jdbc-${TEST_VER}-tests.jar databend-jdbc-tests.jar
-fi
+# Always use local artifacts (built in CI or local dev)
+cp ../../databend-jdbc/target/databend-jdbc-${TEST_VER}-tests.jar databend-jdbc-tests.jar
 
-if [ -z "DATABEND_JDBC_VERSION" ]; then
-    # only for dev
-    cp ../../databend-jdbc/target/databend-jdbc-${JDBC_VER}.jar databend-jdbc.jar
-else
-    curl -sSLfo "./databend-jdbc-${JDBC_VER}.jar" "https://github.com/databendlabs/databend-jdbc/releases/download/v${JDBC_VER}/databend-jdbc-${JDBC_VER}.jar"
-fi
+# Always use local artifacts (built in CI or local dev)
+cp ../../databend-jdbc/target/databend-jdbc-${JDBC_VER}.jar databend-jdbc.jar
 
 export DATABEND_JDBC_VERSION=$JDBC_VER
 java -Dlogback.logger.root=INFO -cp "testng.jar:slf4j-api.jar:databend-jdbc-${JDBC_VER}.jar:databend-jdbc-tests.jar:jcommander.jar:semver4j.jar" org.testng.TestNG testng.xml
